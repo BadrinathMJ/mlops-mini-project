83330f283d39b3cbf81e8cef1c5435993104036c
BadriMJ <mjcode14@gmail.com>
2025-07-20 14:36:18 +0530

    added tests

--- flask-app/app.py
+++ flask-app/app.py
@@ -6,9 +6,15 @@ import dagshub
 import pickle
 mlflow.set_tracking_uri('https://dagshub.com/mjcode14/mlops-mini-project.mlflow')
 dagshub.init(repo_owner='mjcode14', repo_name='mlops-mini-project', mlflow=True)
-
+# load model from model registry
+def get_latest_model_version(model_name):
+    client = mlflow.MlflowClient()
+    latest_version = client.get_latest_versions(model_name, stages=["Production"])
+    if not latest_version:
+        latest_version = client.get_latest_versions(model_name, stages=["None"])
+    return latest_version[0].version if latest_version else None
 model_name = "my_model"
-model_version = 5
+model_version = get_latest_model_version(model_name)
 
 model_uri = f'models:/{model_name}/{model_version}'
 vectorizer = pickle.load(open('models/vectorizer.pkl', 'rb'))
